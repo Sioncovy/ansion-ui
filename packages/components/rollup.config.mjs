c22d7831616b3486747038fb710f0a34469d6778
Sioncovy <sioncovy@gmail.com>
2025-01-23 23:26:15 +0800

    🔧 chore: 样式打包

--- packages/components/rollup.config.mjs
+++ packages/components/rollup.config.mjs
@@ -4,8 +4,15 @@ import typescript from '@rollup/plugin-typescript'
 import postcss from 'rollup-plugin-postcss'
 import babel from '@rollup/plugin-babel'
 import terser from '@rollup/plugin-terser'
+import alias from '@rollup/plugin-alias'
 import { defineConfig } from 'rollup'
 import dts from 'rollup-plugin-dts'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import postcssImport from 'postcss-import'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
 
@@ -26,6 +33,15 @@ const config = defineConfig([
     ],
     external: ['react', 'react-dom', 'classnames'],
     plugins: [
+      // 路径别名配置
+      alias({
+        entries: [
+          {
+            find: '@',
+            replacement: path.resolve(__dirname, 'src')
+          }
+        ]
+      }),
       resolve({ extensions }),
       commonjs(),
       typescript({
@@ -34,22 +50,36 @@ const config = defineConfig([
         declarationDir: 'dist/types',
         exclude: ['**/*.test.tsx', '**/*.test.ts']
       }),
+      postcss({
+        // extract: 'index.css', // 提取CSS到单独文件
+        modules: {
+          generateScopedName: '[name]__[local]__[hash:base64:5]',
+          localsConvention: 'camelCase',
+          exclude: /tokens\/css\/.*\.css$/
+        },
+        use: ['sass'],
+        inject: true, // 自动注入 CSS
+        minimize: true,
+        extensions: ['.css', '.scss'], // 需要处理的文件扩展名
+        // 添加 PostCSS 插件配置
+        plugins: [
+          postcssImport({
+            // 可以配置导入路径
+            path: [
+              'src/styles/tokens/css',
+              'src', // 添加 src 目录
+              '.' // 添加根目录
+            ]
+          })
+        ]
+      }),
       babel({
         extensions,
         babelHelpers: 'bundled',
         presets: ['@babel/preset-env', ['@babel/preset-react', { runtime: 'automatic' }]],
         exclude: 'node_modules/**'
       }),
-      postcss({
-        extract: false, // 不单独提取 CSS 文件
-        modules: {
-          generateScopedName: '[name]__[local]__[hash:base64:5]',
-          localsConvention: 'camelCase'
-        },
-        use: ['sass'],
-        inject: true, // 自动注入 CSS
-        minimize: true
-      }),
+
       terser()
     ]
   },
@@ -66,7 +96,8 @@ const config = defineConfig([
         }
       })
     ],
-    external: [/\.scss$/]
+    // 修改 external 配置，排除所有 CSS/SCSS 文件
+    external: [/\.(css|scss)$/]
   }
 ])
 
--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,2 +1,4 @@
+import './styles/tokens/css/index.css'
+
 export * from './components/button'
 export * from './types'
